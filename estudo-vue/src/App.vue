88d900de881e883907d60d8e3687ffbb1ea8f092
Yonatha <yonathalmeida@gmail.com>
2024-03-14 21:30:16 -0300

    feature/60 Implementar Componente da Questão 3

--- estudo-vue/src/App.vue
+++ estudo-vue/src/App.vue
@@ -11,17 +11,9 @@ export default {
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
+  <nav>
         <RouterLink to="/questoes/3">Questão 3</RouterLink>
-      </nav>
-    </div>
-  </header>
+  </nav>
 
   <RouterView />
 </template>
