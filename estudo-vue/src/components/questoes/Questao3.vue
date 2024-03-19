c09e8449a9538afcaf70e798041197321f08fdb2
Yonatha <yonathalmeida@gmail.com>
2024-03-18 22:07:55 -0300

    Implementar Componente da Questão 3

--- estudo-vue/src/components/questoes/Questao3.vue
+++ estudo-vue/src/components/questoes/Questao3.vue
@@ -1,5 +1,67 @@
 <template>
     <div>
         <h2>Questão 3</h2>
+
+        <label for="">Cliente</label><br>
+        <input v-model="nome" />
+        <br>
+        <label>Descrição Conta</label><br>
+        <input v-model="descricao" />
+        <label>Valor R$</label> <input v-model="valor" />
+        <br>
+
+        <button @click="adicioanrConta">Adicionar</button>
+        <br>
+        <ul>
+            <li v-for="conta in contas">
+                {{ conta.descricao }} R$ {{ conta.valor }}
+            </li>
+        </ul>
+
     </div>
 </template>
+
+
+<script>
+
+export default {
+    data() {
+        return {
+            mensagem: "",
+            nome: "",
+            saldoBancario: 100,
+            contas: [],
+            totalgasto: 0,
+            saldoFinal: 0,
+            descricao: "",
+            valor: null
+        }
+    },
+    methods: {
+        adicioanrConta() {
+            const conta = { descricao: this.descricao, valor: this.valor }
+            this.contas.push(conta)
+            this.limparCamposDeConta()
+        },
+        limparCamposDeConta() {
+            this.descricao = null
+            this.valor = null
+        },
+        somarGastos(listaDeContas) {
+            let total = 0;
+            for (let index = 0; index < listaDeContas.length; index++) {
+                const conta = listaDeContas[index];
+                total += conta;
+            }
+            return total;
+        },
+        exibirMensagem() {
+            if (this.totalgasto <= this.saldoBancario) {
+                this.mensagem = `conseguiu pagar a conta na lanchonete`
+            } else {
+                this.mensagem`saldo insuficiente, falta R$ ${saldoFinal},00 para pagar a conta`
+            }
+        }
+    },
+}
+</script>
