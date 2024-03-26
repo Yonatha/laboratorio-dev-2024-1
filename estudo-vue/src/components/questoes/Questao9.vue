6b9a69d2ee8fe8c0b4a26a8b95554f1a29512317
Yonatha <yonathalmeida@gmail.com>
2024-03-25 22:03:37 -0300

    Code review Q9

--- estudo-vue/src/components/questoes/Questao9.vue
+++ estudo-vue/src/components/questoes/Questao9.vue
@@ -8,8 +8,24 @@
         </p>
 
         <label>Nome do Filme: </label>
+        <br>
         <input type="text" name="nome-filme" v-model="novoFilme" placeholder="Informe o nome do filme...">
-        <button @click="adicionarFilme()">Adicionar</button>
+        <br>
+        <label>Genero</label>
+        <br>
+        <select name="generos" v-model="generoSelecionado">
+            <option>Selecione o Genero...</option>
+            <option v-for="genero in generos" :value="genero">
+                {{ genero }}
+            </option>
+        </select>
+        <br>
+        <label>Situação:</label>
+        <div v-for="situacao in situacoes">
+            <input type="radio" v-model="situacaoSelecionada" :value="situacao"> {{ situacao }}
+        </div>
+
+        <button id="btnAdicionarFilme" @click="adicionarFilme()">Adicionar</button>
 
         <p :class="corMensagem">
             {{ mensagem }}
@@ -25,7 +41,7 @@
                     <h3>Imagem não disponível</h3>
                 </div>
 
-                <span class="titulo">{{ filme.titulo }}</span>
+                <span class="titulo">{{ filme.titulo }} ({{ filme.genero }})</span>
             </li>
         </ul>
 
@@ -41,7 +57,20 @@ export default {
             corMensagem: 'sucesso',
             mensagem: null,
             novoFilme: null,
+            generoSelecionado: null,
+            situacaoSelecionada: "Habilitado",
             qtdFilmes: 0,
+            situacoes: [
+                "Habilitado",
+                "Desabilitado",
+                "Cancelado",
+                "Arquivado"
+            ],
+            generos: [
+                "terror",
+                "comedia",
+                "ação"
+            ],
             filmesFavoritos: [
                 {
                     codigo: 1,
@@ -76,13 +105,14 @@ export default {
                 {
                     codigo: this.filmesFavoritos.length + 1,
                     titulo: this.novoFilme,
-                    capa: null
+                    capa: null,
+                    genero: this.generoSelecionado
                 }
 
                 const encontrado = this.filmesFavoritos.filter(f => f.titulo == this.novoFilme)
                 if (encontrado.length == 0) {
                     this.filmesFavoritos.push(filme);
-                    this.mensagem = `Filme "${this.novoFilme}" adicionado a lista`; 
+                    this.mensagem = `Filme "${this.novoFilme}" adicionado a lista`;
                 } else {
                     this.mensagem = `Filme "${this.novoFilme}" ja existe na sua lista`;
                 }
@@ -94,9 +124,9 @@ export default {
             }
         },
         validarFormulario() {
-            if (this.novoFilme == null || this.novoFilme.length == 0){
+            if (this.novoFilme == null || this.novoFilme.length == 0) {
                 this.corMensagem = 'falha'
-                 return false
+                return false
             }
 
             this.corMensagem = 'sucesso'
@@ -110,6 +140,10 @@ export default {
 <style>
 body {
     font-family: Arial;
+}
+
+label {
+    font-weight: bold;
 }
 
 .imagem-nao-localizada {
@@ -145,4 +179,16 @@ ul li span.titulo {
 .falha {
     color: orange;
 }
+
+#btnAdicionarFilme {
+    background: green;
+    color: #ffffff;
+    border: none;
+    width: 100px;
+    height: 28px;
+    margin-top: 8px;
+    border-radius: 26px;
+    cursor: pointer;
+}
+
 </style>
